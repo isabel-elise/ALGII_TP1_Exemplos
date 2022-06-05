03388672d08bf357eadae6ed81fb6952b6628ccf
Isabel Elise <isaelised@gmail.com>
2022-06-05 16:19:09 -0300

    Adição dos arquivos de exemplo

---
+++ executar_exemplos.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+./main.py -c ex1.txt
+./main.py -x ex1.z78 -o ex1_out.txt
+
+./main.py -c ex2.txt
+./main.py -x ex2.z78 -o ex2_out.txt
+
+./main.py -c ex3.txt
+./main.py -x ex3.z78 -o ex3_out.txt
+
+./main.py -c ex4.txt
+./main.py -x ex4.z78 -o ex4_out.txt
+
+./main.py -c ex5.txt
+./main.py -x ex5.z78 -o ex5_out.txt
+
+./main.py -c ex6.txt
+./main.py -x ex6.z78 -o ex6_out.txt
+
+./main.py -c ex7.txt
+./main.py -x ex7.z78 -o ex7_out.txt
+
+./main.py -c ex8.txt
+./main.py -x ex8.z78 -o ex8_out.txt
+
+./main.py -c ex9.txt
+./main.py -x ex9.z78 -o ex9_out.txt
+
+./main.py -c ex10.txt
+./main.py -x ex10.z78 -o ex10_out.txt
